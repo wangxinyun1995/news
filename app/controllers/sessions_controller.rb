6ceec5eb85aac6bdda28321ba62662df5abcedbe
yangfeng199555 <329414837@qq.com>
2019-03-07 23:35:11 +0800

    fix(注册完善):

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+     # 创建一个错误消息
+     flash[:danger] = '邮箱/密码不正确' # 不完全正确
+     render 'new'
+   end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
