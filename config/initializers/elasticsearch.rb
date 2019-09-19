110c624890abc696aba1819b1b1e76d66e138ea4
王新云 <329414837@qq.com>
2019-09-19 15:19:35 +0800

    fix(view): 解决报错

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
 #elastic_urls = ["localhost:9200"]
-elastic_urls = [Setting.ali_url]
+elastic_urls = ["#{Setting.ali_url}"]
 Searchkick.client = Elasticsearch::Client.new(hosts: elastic_urls, retry_on_failure: true)
