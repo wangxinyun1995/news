c1cafeebddabced80d63705dc6024897af325714
王新云 <329414837@qq.com>
2020-03-19 10:14:54 +0800

    fix(view): 执行rake任务

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,5 +1,5 @@
 if Setting.is_development
-  GrapeSwaggerRails.options.url = "swagger_doc"
-  GrapeSwaggerRails.options.app_name = 'News'
-  GrapeSwaggerRails.options.app_url = '/api/'
+  # GrapeSwaggerRails.options.url = "swagger_doc"
+  # GrapeSwaggerRails.options.app_name = 'News'
+  # GrapeSwaggerRails.options.app_url = '/api/'
 end
